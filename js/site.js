155f04516be326c9a643e396451e91251714e542
Rating001 <106230549+Rating001@users.noreply.github.com>
2022-08-21 00:10:54 -0700

    Began the coding challenge

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//Get the string from the user input
+//Control Function
+function getInput() {
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+//Reverse the string
+//Logic Function
+function reverseString(userString) {
+
+    let revString = [];
+
+    //Reverse a string using a for loop
+    for (let index = (userString.length - 1); index >= 0; index--) {
+        revString += userString[index];
+        }
+
+    return revString;
+
+}
+
+//Display the reversed string on the msg
+//View Function
+function displayReverse(revString) {
+
+    
 }
